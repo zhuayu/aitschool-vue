6c4c069ff79be95dc6b95178dcec0d8f414a0ff5
Jax Chu <294242589@qq.com>
2019-09-05 12:09:08 +0800

    update beian

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="page--footer">
-    <div class="page--footer__container">Copyright © 2018 扎克斯体验技术部出品 京ICP备17033615号-2</div>
+    <div class="page--footer__container">Copyright © 2018 扎克斯体验技术部出品 
+      粤ICP备18089899号-3 
+      <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011802000279" target="_blank">
+        <img style="width: 16px;height: 16px;vertical-align: middle;margin-left: 4px;" class="beian-img" src="http://www.beian.gov.cn/img/ghs.png">
+      </a>
+      粤公网安备 44011802000279号</div>
   </div>
 </template>
 
